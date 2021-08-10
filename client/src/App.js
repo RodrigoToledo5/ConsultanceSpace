950788f418d0bee0aa121595a64bd632344f8f6e
rodrigo toledo <rodrit182@hotmail.com>
2021-08-10 15:59:20 -0300

    route added for nav

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {Route,Switch} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
-import Nav from './components/Header/nav';
+import Nav from './components/Header/Nav';
 
 function App() {
   return (
