1d10c85dbb71f92350a8ebbf6e81fdc2008f45a3
Your Name <you@example.com>
2021-08-10 17:55:21 -0300

    modelo paciente

---
+++ api/src/models/Paciente.js
@@ -0,0 +1,50 @@
+const {DataTypes,Sequelize} = require('sequelize');
+//Exportamos por modulo la funcion que define el modelo de las actividades
+module.exports = (sequelize) => {
+	//* defino el modelo
+	sequelize.define('paciente', {
+		DNI: {
+			type: DataTypes.BIGINT,
+			allowNull: false,
+			primaryKey: true,
+		},
+		nombre: {
+			type: DataTypes.STRING,
+			allowNull : false,
+		},
+		apellidos:{
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		telefono:{
+			type : DataTypes.BIGINT,
+			allowNull: false,
+		},
+		fecha_de_nacimiento:{
+			type: DataTypes.DATE,
+			allowNull: false,
+		},
+		direccion:{
+			type: DataTypes.TEXT,
+			allowNull : false
+		},
+		pais:{
+			type: DataTypes.STRING,
+			allowNull: false
+		},
+		email:{
+			foreignKey:true,
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate:{
+				isEmail: true
+			}
+		},
+		id_Historia_Clinica:{
+			type: DataTypes.BIGINT,
+			allowNull: false,
+		}
+	},{
+		timestamps: false,
+	});
+};
