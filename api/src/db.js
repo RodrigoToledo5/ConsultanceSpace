11b1ced9da43a68c01dd632b4d5da87d4f8bff21
Jorge Gutierrez <jorge.gutierrezl@outlook.com>
2021-08-10 15:54:02 -0500

    usuario y profecional

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/consultas`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -32,15 +32,15 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Country, Activity } = sequelize.models;
+const { Usuario, Profesional } = sequelize.models;
 
 
 ///aqui van las relaciones
-Country.belongsToMany(Activity, {through: 'CountryActivity'});
-Activity.belongsToMany(Country, {through: 'CountryActivity'});
+/* Country.belongsToMany(Activity, {through: 'CountryActivity'});
+Activity.belongsToMany(Country, {through: 'CountryActivity'}); */
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
   conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
-  Activity,Country
+  
 };
