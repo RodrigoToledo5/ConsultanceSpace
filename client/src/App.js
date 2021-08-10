ded44a95faef429e43e15ae056de948ef3adad3a
LAPTOP-MP5E825G\Francisco <ftbr0011@gmail.com>
2021-08-10 16:24:26 -0300

    init commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import {Route,Switch} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
 import Nav from './components/Header/Nav';
+import Carousel from './components/Landing/Carousel/Carousel';
 
 function App() {
   return (
     <>
-      <Switch>
-        <Route path="/" component={Nav}/>
-      </Switch>
+        <Nav />
+        <Carousel />
     </>
   );
 }
