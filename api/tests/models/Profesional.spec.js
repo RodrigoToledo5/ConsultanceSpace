c637fb55efca5e870ed85488d1b7248cd910fd46
Your Name <you@example.com>
2021-08-11 17:30:47 -0300

    modificando test profesional

--- api/tests/models/Profesional.spec.js
+++ api/tests/models/Profesional.spec.js
@@ -17,6 +17,8 @@ describe('Profesional', () => {
                 Profesional.create({
                     nombre: "nombre",
                     apellidos: 'apellidos',
+                    telefono: 12312424124,
+                    direccion: 'calle desconocida 234'
                 })
                 .then(() => done(new Error('Se requiere una cedula valida')))
                 .catch(() => done());
@@ -32,6 +34,8 @@ describe('Profesional', () => {
                 Profesional.create({
                     cedula: 13232323,
                     apellidos: 'apellidos',
+                    telefono: 12312424124,
+                    direccion: 'calle desconocida 234'
                 })
                 .then(() => done(new Error('Se requiere un "nombre" valido')))
                 .catch(() => done());
@@ -47,12 +51,48 @@ describe('Profesional', () => {
                 Profesional.create({
                     nombre: 'nombre',
                     cedula: 13232323,
+                    telefono: 12312424124,
+                    direccion: 'calle desconocida 234'
                 })
                 .then(() => done(new Error('Se requiere "apellidos" valido')))
                 .catch(() => done());
             });
             it('Deberia dar error si solo se ingresa "apellidos"', (done) => {
                 Profesional.create({ apellidos: 'apellidos' })
+                .then(() => done(new Error('Se requiere mas datos')))
+                .catch(() => done());
+            });
+        });
+        describe('telefono', () => {
+            it('Deberia dar un error si telefono es null', (done) => {
+                Profesional.create({
+                    cedula: 1212123,
+                    nombre: 'nombre',
+                    cedula: 13232323,
+                    direccion: 'calle desconocida 234'
+                })
+                .then(() => done(new Error('Se requiere un "telefono" valido')))
+                .catch(() => done());
+            });
+            it('Deberia dar error si solo se ingresa "telefono"', (done) => {
+                Profesional.create({ telefono: 12122332 })
+                .then(() => done(new Error('Se requiere mas datos')))
+                .catch(() => done());
+            });
+        });
+        describe('direccion', () => {
+            it('Deberia dar un error si direccion es null', (done) => {
+                Profesional.create({
+                    cedula: 1122354,
+                    nombre: 'nombre',
+                    cedula: 13232323,
+                    telefono: 12312424124,
+                })
+                .then(() => done(new Error('Se requiere una "direccion" valida')))
+                .catch(() => done());
+            });
+            it('Deberia dar error si solo se ingresa "apellidos"', (done) => {
+                Profesional.create({ direccion: 'direccion desconocida 123' })
                 .then(() => done(new Error('Se requiere mas datos')))
                 .catch(() => done());
             });
