970b4afaad73b597e00fa3d907a6e7b54216aaa0
LAPTOP-MP5E825G\Francisco <ftbr0011@gmail.com>
2021-08-11 11:18:30 -0300

    for Merge

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,9 +1,8 @@
 import {Route,Switch} from 'react-router-dom'
 import Button from '@material-ui/core/Button'
 import SaveIcon from '@material-ui/icons/Save'
-import Nav from '../components/Header/Nav';
-import ReactCarousel from '../components/Landing/Carousel/Carousel';
-import CallToAction from '../components/Landing/CallToAction/CallToAction';
+import ReactCarousel from '../Landing/Carousel/Carousel';
+import CallToAction from '../Landing/CallToAction/CallToAction';
 import {  Box } from '@material-ui/core';
 
 function Main() {
@@ -12,7 +11,7 @@ function Main() {
         <Box display="flex" justifyContent="center" padding="10px" width="100%">
           <Box width="60%" margin="10px"><ReactCarousel height={400}/></Box>
           <Box width="30%" margin="10px"><CallToAction height={400}/></Box>
-        </Box>
+        </Box> 
     </>
   );
 }
