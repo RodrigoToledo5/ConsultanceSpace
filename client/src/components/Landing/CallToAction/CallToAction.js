ed32b5872d60fd796dba62c34d4de465a6071c83
LAPTOP-MP5E825G\Francisco <ftbr0011@gmail.com>
2021-08-11 12:18:36 -0300

    fixed sign-in button in CallToAction component

--- client/src/components/Landing/CallToAction/CallToAction.js
+++ client/src/components/Landing/CallToAction/CallToAction.js
@@ -1,9 +1,9 @@
 // CallToAction export default, height can be passed by params, text
-// is not responsive 
+// is not responsive
 
 import { Box, Button, Paper, Grid, Typography } from "@material-ui/core";
 import React from "react";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import CalendarIcon from "@material-ui/icons/CalendarToday";
 
 export default function CallToAction({ height }) {
@@ -25,21 +25,34 @@ export default function CallToAction({ height }) {
     },
   };
 
+  let history = useHistory();
+
+  function handleClick(navlink) {
+    history.push(navlink);
+  }
+
   return (
     <Box>
       <Paper elevation={3} style={styles.paperContainer}>
         <Typography variant="h4">Somos ConsultanceSpace!</Typography>
         <Typography variant="body1">
-          <p>Una plataforma de reseva y gestion de turnos, pagos y stock que te va
-          a sorprender.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ut convallis justo. </p>
+          <p>
+            Una plataforma de reseva y gestion de turnos, pagos y stock que te
+            va a sorprender. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit. Vivamus ut convallis justo.{" "}
+          </p>
         </Typography>
         <Grid container style={styles.grid}>
-          <Link to="/sign-In">
-            <Button color="primary" style={styles.button} startIcon={<CalendarIcon />}>
-              Reserva ya
-            </Button>
-          </Link>
+          <Button
+            color="primary"
+            style={styles.button}
+            startIcon={<CalendarIcon />}
+            onClick={() => {
+              handleClick("/sign-in");
+            }}
+          >
+            Reserva ya
+          </Button>
         </Grid>
       </Paper>
     </Box>
