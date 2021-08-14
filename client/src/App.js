05768c5edc26870cf70ce6637500209155e71a0f
Your Name <you@example.com>
2021-08-14 20:38:54 -0300

    login y cambiar contraseña

--- client/src/App.js
+++ client/src/App.js
@@ -7,12 +7,14 @@ import Log from './components/Log/Log';
 import Lorem from './components/Main/Lorem';
 import Footer from './components/Footer/Footer';
 import Main from './components/Main/Main';
+import ResetPassword from './components/ResetPassword/ResetPassword';
 
 function App() {
   return (
     <> 
       <Route path="/sign-in" component={Sign}/>
       <Route path="/login" component={Log}/>
+      <Route path="/reset-password" component={ResetPassword}/>
       <Route path="/" component={Nav}/>
       <Route path="/home" component={Main}/>
       <Route exact path="/" component={Lorem}/>
