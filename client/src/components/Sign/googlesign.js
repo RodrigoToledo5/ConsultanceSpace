d336fdd67f5e36baabb5549fd0173834768185ed
jescabvaz <83148582+jescabvaz@users.noreply.github.com>
2021-08-16 22:08:52 -0500

    form google

--- client/src/components/Sign/googlesign.js
+++ client/src/components/Sign/googlesign.js
@@ -15,30 +15,56 @@ const useStyles = makeStyles((theme)=>({
     "& .MuiInputBase-root": {
         color: "#2196f3 !important"
     },
+    title:{
+        color: '#159DE9',
+        fontFamily: 'Roboto'
+    },
+    principal:{
+        marginTop: "100px",
+        display: "flex",
+        justifyContent: "center",
+
+    },
     divStyle:{
         paddingTop: '5%',
         backgroundColor: '#E8EEF4',
-        width: "50%",
-        marginLeft: "25%",
+        width: "60%",
+        borderRadius: "10px",
         marginTop: "85px",
         marginBottom: "5%",
         paddingBottom: '5%',
         display:"flex",
         flexDirection:"column",
-        alignItems:"center"
+        alignItems:"center",
+        justifyContent: "center",
+        "@media (max-width : 500px)": {
+            width: "100%",
+          },
     },
     firstGrid:{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
+        "@media (max-width : 500px)": {
+            marginLeft:"8%",
+          },
 
     },
     margin: {
         margin: theme.spacing(2),
     },
     textField: {
-        width: '80%',
+        width: '100%',
         main: blue[500],
+    },
+    form:{
+        marginRight: "20px",
+    },
+    formAdress:{
+        marginLeft: "10px",
+        "@media (max-width : 500px)": {
+            marginLeft: "0",
+          },
     },
     labelTextField:{
         color: "#2196f3 !important",
@@ -48,8 +74,7 @@ const useStyles = makeStyles((theme)=>({
         minWidth: 120,
     },
     selectEmpty:{
-        width: "191px",
-        marginRight: "5px",
+        width: "230px",
         main: "#2196f3 !important",
     },
     alert:{
@@ -58,16 +83,31 @@ const useStyles = makeStyles((theme)=>({
         justifyContent:"center",
         textAlign:"center"
     },
+    country :{
+        display: "flex",
+        flexDirection: "column",
+        "@media (max-width : 500px)": {
+            display: "inline-block",
+          },
+        
+
+    },
     slider:{
         color: "#2196f3",
         width:"90%",
     },
     boxSlider:{
-        width:"40%",
+        width:"60%",
+        marginLeft:"20%",
         display:'flex',
         flexDirection:'column',
         alignItems: "center",
         minWidth: "200px",
+        '@media (max-width : 500px)':{
+            width:"100%",
+            marginLeft:"0",
+            
+        }
     },
     boxSliderText:{
         width:"100%",
@@ -78,19 +118,20 @@ const useStyles = makeStyles((theme)=>({
         }
     },
     fontNormal:{
-        fontSize:"15px",
+        fontSize:"20px",
         '@media (max-width : 500px)':{
-            fontSize:"10px",
+            fontSize:"15px",
         }
     },
     fontSelect:{
-        fontSize:"15px",
+        fontSize:"20px",
         color:"#2196f3",
         '@media (max-width : 500px)':{
-            fontSize:"10px",
+            fontSize:"15px",
         }
     }
 }));
+
 
 
 export default function GoogleSign(){
@@ -185,27 +226,35 @@ export default function GoogleSign(){
         }
     }
     const alertFunction =() => {
-        if(
-            load==="cargando"
-            ){
-                return (
-                   <Box width="100%" justifyContent="center">
-                       <Alert className={classes.alert} severity="success">      
-                             Campos completos
-                       </Alert>
+        
+        if(patient.name && patient.dni && patient.lastName && patient.phone &&
+            patient.birth && patient.address && patient.country){
+            return (
+               <Box width="100%" justifyContent="center">
+                   <Alert className={classes.alert} severity="success">      
+                         Campos completos
+                   </Alert>
+               </Box>
+            )}
+            else if(load === ""){
+                return(
+                   <Box width="100%" height="50px" justifyContent="center">
+                       <Alert className={classes.alert} severity="info">      
+                            Complete todos los campos
+                        </Alert>
                    </Box>
-                )}
-                else{
-                    return(
-                       <Box width="100%" height="50px" justifyContent="center">
-                           <Alert className={classes.alert} severity="info">      
-                                Complete todos los campos
-                            </Alert>
-                       </Box>
-                    )
-                }
-        }
-    
+                )
+            }
+            else {
+                return(
+                    <Box width="100%" height="50px" justifyContent="center">
+                        <Alert className={classes.alert} severity="success">      
+                             Registro exitoso
+                         </Alert>
+                    </Box>
+                 ) 
+            }
+    }
     useEffect(()=>{
         dispatch(getCountries());
     },[dispatch])
@@ -213,6 +262,7 @@ export default function GoogleSign(){
     return (
         <>
             <Container className={classes.divStyle}>
+            <h2 className={classes.title}>Registrate</h2>
             {alertFunction()}
             <form onSubmit={onHandleSubmit}>
             <Box className={clsx(classes.margin, classes.boxSlider)}>
@@ -239,7 +289,7 @@ export default function GoogleSign(){
                     
                     
                         <Grid item md={12}>
-                            <FormControl>
+                            <FormControl className={classes.form}>
                                 <TextField
                                     label="No. de Identificación"
                                     id="dni"
@@ -255,7 +305,7 @@ export default function GoogleSign(){
                                     
                                 />
                             </FormControl>
-                            <FormControl>
+                            <FormControl className={classes.form}>
                                 <TextField
                                     label="Nombre"
                                     id="name"
@@ -272,7 +322,7 @@ export default function GoogleSign(){
                             </FormControl>
                         </Grid>
                         <Grid item md={12}>
-                            <FormControl>
+                            <FormControl className={classes.form}>
                                 <TextField
                                     label="Apellido"
                                     id="lastName"
@@ -287,7 +337,7 @@ export default function GoogleSign(){
                                     autoComplete="off"
                                 />
                             </FormControl>
-                            <FormControl>
+                            <FormControl className={classes.form}>
                                 <TextField
                                     label="Telefono"
                                     id="phone"
@@ -306,7 +356,7 @@ export default function GoogleSign(){
                         </Grid>
                         <Grid item md={12}>
                             
-                            <FormControl>
+                            <FormControl className={classes.form}>
                                 <TextField
                                     // helperText="Fecha de nacimiento"
                                     id="birth"
@@ -321,7 +371,7 @@ export default function GoogleSign(){
                                     helperText={errors.birth}
                                 />
                             </FormControl>
-                            <FormControl>
+                            <FormControl className={classes.form}>
                                 <TextField
                                     label="Dirección"
                                     id="address"
@@ -337,7 +387,7 @@ export default function GoogleSign(){
                             </FormControl>
                         </Grid> 
                         <Grid item md={12}>
-                            <FormControl variant="outlined" className={classes.formControl}>
+                            <FormControl variant="outlined" className={clsx(classes.formControl, classes.country)}>
                                 <InputLabel id="demo-simple-select-outlined-label">Selecciona tu pais</InputLabel>
                                 <Select
                                     label="Selecciona tu pais"
