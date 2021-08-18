dc6dc903e6cd7e255290d2efc9af7e5217fc7147
jescabvaz <83148582+jescabvaz@users.noreply.github.com>
2021-08-18 17:22:07 -0500

    buscador por nombre del paciente

--- api/src/routes/Patients.js
+++ api/src/routes/Patients.js
@@ -6,15 +6,9 @@ const { Op} = require("sequelize");
 router.get('/patients', async (req, res, next) => { 
     try {
         const {nombre} = req.query;
-        console.log(nombre)
-        const patient = await Paciente.findAll({
-            where:{
-                nombre:{
-                    [Op.iLike]: `%${nombre}%`
-                }
-            }
-        })
-        return patient ? res.json(patient) : res.send("Paciente no encontrado")
+        const patient = await Paciente.findAll()
+        const patientFound = patient.filter((elemento)=>elemento.fullName === nombre.toUpperCase())
+        return patient ? res.json(patientFound) : res.send("Paciente no encontrado")
         
     } catch (err) {
         next(err) 
