488e6e18813dd320a967ef10ce76ae0f2be2b73a
MarlonLondono <rudita.2001.258@gmail.com>
2021-08-18 21:19:23 -0500

    creacion de profesional pacientes

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   console.log("Loaded");
   server.listen(3001, () => {
     console.log("%s listening at 3001");
