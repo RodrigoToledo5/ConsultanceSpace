0a008584b64d16c7b1be73dbe70fc20bcf6fd65e
MarlonLondono <rudita.2001.258@gmail.com>
2021-08-19 10:23:42 -0500

    Filter patients completed

--- client/src/components/Professional/SubComponents/Patients/Patients.js
+++ client/src/components/Professional/SubComponents/Patients/Patients.js
@@ -1,6 +1,7 @@
 import {Box, Typography, makeStyles } from "@material-ui/core";
 import { blue } from "@material-ui/core/colors";
 import { DataGrid } from '@material-ui/data-grid';
+import { useSelector } from "react-redux";
 import SearchBar from "../../SearchBar";
 
 const useStyle = makeStyles((theme) => ({
@@ -24,70 +25,75 @@ const useStyle = makeStyles((theme) => ({
       
     }
 }));
-
-  const columns = [
-    { field: 'DNI', headerName: 'DNI', width: 110 },
-    {
-      field: 'Nombre',
-      headerName: 'Nombre',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'Apellido',
-      headerName: 'Apellido',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'Telefono',
-      headerName: 'Telefono',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'Fecha de nacimiento',
-      headerName: 'Fecha de nacimiento',
-      type: 'date',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'Direccion',
-      headerName: 'Direccion',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'Pais',
-      headerName: 'Pais',
-      width: 150,
-      editable: true,
-    },
-    {
-      field: 'Email',
-      headerName: 'Email',
-      width: 150,
-      editable: true,
-    },
-  ];
-  
-
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
   
 export default function Patients(){
     const classes = useStyle();
+    const patients = useSelector(store => store.reducerSearchPatients.patients);
+    console.log(patients);
 
+    const columns = [
+      { field: 'id', headerName: 'ID', width: 110 },
+      { field: 'dni', headerName: 'DNI', width: 110 },
+      {
+        field: 'name',
+        headerName: 'Nombre',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'lastName',
+        headerName: 'Apellido',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'phone',
+        headerName: 'Telefono',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'birth',
+        headerName: 'Fecha de nacimiento',
+        type: 'date',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'address',
+        headerName: 'Direccion',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'country',
+        headerName: 'Pais',
+        width: 150,
+        editable: true,
+      },
+      {
+        field: 'email',
+        headerName: 'Correo electronico',
+        width: 150,
+        editable: true,
+      },
+    ];
+    
+    // const rows = [];
+    const rows = patients && patients.map(patient => {
+      return {
+        id: patient.id,
+        dni: patient.DNI,
+        name: patient.nombre,
+        lastName: patient.apellidos,
+        phone: patient.telefono,
+        birth: patient.fecha_de_nacimiento,
+        address: patient.direccion,
+        country: patient.pais,
+        email: patient.usuarioEmail
+      }
+    });
+    
     return(
         <Box className={classes.box}>
 
