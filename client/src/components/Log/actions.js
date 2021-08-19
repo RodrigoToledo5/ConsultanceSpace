bacbee8b413b64c163dc87536c2e1c18b3069b23
LAPTOP-MP5E825G\Francisco <ftbr0011@gmail.com>
2021-08-19 13:28:47 -0300

    added redux request info user

--- client/src/components/Log/actions.js
+++ client/src/components/Log/actions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const POST_LOG = 'POST_LOG';
+export const INFO_USER = 'INFO_USER';
 export const DESLOG = 'DESLOG';
 
 const api = 'http://localhost:3001';
@@ -14,6 +15,16 @@ export const postLogIn = (email)=>{
         })
     console.log(res.data);
     dispatch({ type: POST_LOG, payload:  res.data })
+}}
+
+export const getInfo = (user)=>{
+    return async (dispatch) => {
+    const res = await axios({
+            method: 'POST',
+            url: `${api}/info`,
+            data: user
+        })
+    dispatch({ type: INFO_USER, payload:  res.data })
 }}
 
 export const desLog=()=>(dispatch)=>{
--- client/src/components/Patient/PatientsDashboard.js
+++ client/src/components/Patient/PatientsDashboard.js
@@ -1,6 +1,9 @@
 import { Box, makeStyles } from "@material-ui/core";
 import NavPanel from "./NavPanel/NavPanel";
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import { getInfo } from "../Log/actions";
+
 const useStyle=makeStyles(theme=>({
     menu:{
         margin: theme.spacing(1),
@@ -38,6 +41,11 @@ const useStyle=makeStyles(theme=>({
 export default function PatientsDashboard(){
     const classes=useStyle();
     const [actComponent, updateComponent] = useState(null);
+    const user = useSelector((store) => store.reducerLog.user);
+    const dispatch = useDispatch();
+    useEffect(()=>{
+        dispatch(getInfo({...user}));
+    },[])
     return(
         <Box className={classes.box}>
             <NavPanel updateComponent={updateComponent}/>
