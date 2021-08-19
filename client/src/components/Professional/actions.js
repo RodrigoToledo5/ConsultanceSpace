11ff248798efdbb62e8e243f83f07e4bc79f555b
rodrigo toledo <rodrit182@hotmail.com>
2021-08-19 13:36:06 -0300

    action from profesional done

--- client/src/components/Professional/actions.js
+++ client/src/components/Professional/actions.js
@@ -2,16 +2,19 @@ import axios from "axios";
 const api = 'http://localhost:3001';
 export const GET_PROFESIONAL = 'GET_PROFESIONAL';
 export const GET_PATIENT = 'GET_PATIENT';
-export const POST_SIGN_IN = 'POST_SIGN_IN';
 
-export const searchprofesional = (fullname)=>{
-    return async (dispatch) => {
-    const res = await axios({
-            method: 'GET',
-            url: `${api}/profesional`,
-            data: {nombre:fullname}
-        })
-    dispatch({ type: POST_SIGN_IN, payload:  res.data })
+export const searchprofesional = (input)=>{
+    if(input.includes('@')){
+        return async (dispatch) => {
+            const res = await axios.get(`${api}/profesional?email=${input}`)
+            dispatch({ type: GET_PROFESIONAL, payload:  res.data })
+        }
+    }
+    else{
+        return async (dispatch) => {
+        const res = await axios.get(`${api}/profesional?fullName=${input}`)
+        dispatch({ type: GET_PROFESIONAL, payload:  res.data })
+        }
 }}
 
 export const getPatient = (name) => {
