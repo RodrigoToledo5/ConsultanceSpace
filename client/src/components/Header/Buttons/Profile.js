6eef230b1bac76d3b6ee0e403db387dda5c70355
rodrigo toledo <rodrit182@hotmail.com>
2021-08-21 13:29:51 -0300

    updated

--- client/src/components/Header/Buttons/Profile.js
+++ client/src/components/Header/Buttons/Profile.js
@@ -1,11 +1,14 @@
 import {Button,Box, FormControl,MenuItem,InputLabel,Select} from '@material-ui/core/'
-import { makeStyles,TextField} from '@material-ui/core';
+import { makeStyles,TextField,Typography} from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import { blue, red} from '@material-ui/core/colors';
 import { useFirebaseApp, useUser } from "reactfire";
 import { getCountries } from '../../Sign/actions';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import validate from '../../Sign/functions/validate';
+import { updateProfile } from './redux/actionUpdate';
+
 const useStyle=makeStyles(theme=>({
     btn:{
         margin: theme.spacing(1),
@@ -29,7 +32,7 @@ const useStyle=makeStyles(theme=>({
         padding:"10px",
         marginLeft:"20px",
         marginRight:"50px",
-        backgroundColor:"#C4C4C4",
+        backgroundColor:"rgb(232, 240, 254) ",
         borderRadius:"10px",
     },
     pick:{
@@ -76,21 +79,85 @@ export default function Profile({onClick}){
     const classes=useStyle();
     const user=useUser();
     const countries = useSelector((state)=> state.reducerSign.countries);
+    const profile = useSelector((state) => state.reducerLog.info);
+    const [onfocusname,setOnFocusName]=useState(false);
+    const [onfocuslastname,setOnFocusLastName]=useState(false);
+    const [onfocusphone,setOnFocusPhone]=useState(false);
+    const [onfocusbirth,setOnFocusBirth]=useState(false);
+    const [onfocusaddres,setOnFocusAdress]=useState(false);
+
+    const [load, setLoad] = useState("");
+    const [errors, setErrors] = useState({});
     const dispatch = useDispatch();
     
+
+    const [patient, setPatient] = useState({
+        dni: '',
+        name: '',
+        lastName: '',
+        email: user.data.email,
+        phone: '',
+        birth: '',
+        address:'',
+        country: '',
+        id:'',
+    });
     
     
     useEffect(()=>{
         dispatch(getCountries());
     },[dispatch])
- 
+    
+
+
+
+    const onHandleChange = (e) => {
+        setPatient({
+            ...patient,
+            [e.target.name]: e.target.value
+        })
+        setErrors(validate({
+            ...patient,
+            [e.target.name]: e.target.value
+          }));
+    }
+
+    const onHandleSubmit = async (e) => {
+        
+        
+        dispatch(updateProfile({...patient,email:profile.usuarioEmail,id:profile.id}));
+        setPatient({
+                dni: '',
+                name: '',
+                lastName: '',
+                email:'',
+                phone: '',
+                birth: '',
+                address:'',
+                country: '',
+                id:'',
+            })
+        setLoad("cargando");
+        
+       
+    }
+    
     return(
+        <form onSubmit={onHandleSubmit}>
+            
         <Box className={classes.menu}>
+            <Typography className={classes.labelTextField}>
+                     Datos personales
+            </Typography>
                 <FormControl className={classes.form}>
                     <TextField
                         className={classes.textfield}
+                        value={patient.name}
+                        onChange={(event)=>onHandleChange(event)}
+                        label={!onfocusname?profile.nombre:"Nombre"}
+                        onFocus={()=>setOnFocusName(!onfocusname)}
+                        onBlur={()=>setOnFocusName(!onfocusname)}
                         type="name"
-                        label="Nombre"
                         variant="outlined"
                         color="secondary"
                         name="name"
@@ -103,19 +170,25 @@ export default function Profile({onClick}){
                 <FormControl className={classes.form}>
                     <TextField
                         className={classes.textfield}
-                        type="lastname"
-                        label="Apellido"
+                        value={patient.lastName}
+                        onChange={(event)=>onHandleChange(event)}
+                        label={!onfocuslastname?profile.apellidos:"Nombre"}
+                        onFocus={()=>setOnFocusLastName(!onfocuslastname)}
+                        onBlur={()=>setOnFocusLastName(!onfocuslastname)}
+
                         variant="outlined"
                         color="secondary"
-                        name="lastname"
+                        name="lastName"
+                        id="lastName"
                         InputProps={{
                         className: classes.input,
                         }}
                         autoComplete="off"
                         />
                 </FormControl>
-                <FormControl className={classes.form}>
+                {/* <FormControl className={classes.form}>
                     <TextField
+                    
                         className={classes.textfield}
                         type="email"
                         label="Correo electronico"
@@ -127,15 +200,19 @@ export default function Profile({onClick}){
                         }}
                         autoComplete="off"
                         />
-                </FormControl>
+                </FormControl> */}
                 <FormControl className={classes.form}>
                     <TextField
                         className={classes.textfield}
-                        type="Telefono"
-                        label="Telefono"
+                        value={patient.phone}
+                        onChange={(event)=>onHandleChange(event)}
+                        label={!onfocusphone?profile.telefono:"Telefono"}
+                        onFocus={()=>setOnFocusPhone(!onfocusphone)}
+                        onBlur={()=>setOnFocusPhone(!onfocusphone)}
+                        type="tel"
                         variant="outlined"
                         color="secondary"
-                        name="Telefono"
+                        name="phone"
                         InputProps={{
                         className: classes.input,
                         }}
@@ -145,11 +222,18 @@ export default function Profile({onClick}){
                 <FormControl className={classes.form}>
                     <TextField
                         className={classes.textfield}
+                        value={patient.birth}
+                        onChange={(event)=>onHandleChange(event)}
+                        //label={!onfocusbirth?profile.fecha_de_nacimiento.substring(0,profile.fecha_de_nacimiento.length-14):""}
+                        // onBlur={()=>setOnFocusBirth(!onfocusbirth)}
+                        onFocus={()=>setOnFocusBirth(false)}
+                        onBlur={()=>setOnFocusBirth(true)}
                         id="birth"
                         type="date"
                         variant="outlined"
                         color="secondary"
-                        name="Telefono"
+                        name="birth"
+                        placeholder="sadasdsad"
                         InputProps={{
                         className: classes.input,
                         }}
@@ -190,9 +274,10 @@ export default function Profile({onClick}){
                                     })}                           
                         </Select>
                 </FormControl>
-                <Button className={classes.head}>
+                <Button type="submit" className={classes.head}>
                     Aceptar
                 </Button>
         </Box>
+        </form>
     )
 }
