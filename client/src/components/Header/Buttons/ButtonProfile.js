f52260788ec63bad4aaa391f81a0484fd80dcc50
rodrigo toledo <rodrit182@hotmail.com>
2021-08-21 17:01:59 -0300

    update profile

--- client/src/components/Header/Buttons/ButtonProfile.js
+++ client/src/components/Header/Buttons/ButtonProfile.js
@@ -1,14 +1,22 @@
 import Button from '@material-ui/core/Button'
-import { makeStyles} from '@material-ui/core';
+import { Box, makeStyles} from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import {useUser,useFirebaseApp } from "reactfire";
 import { useState } from 'react';
+import Alert from '@material-ui/lab/Alert';
 const useStyle=makeStyles(theme=>({
     btn:{
         margin: theme.spacing(1),
         minWidth:'84px'
         
     },
+    alert:{
+        position:"fixed",
+        left:theme.spacing(40),
+        right:theme.spacing(40),
+        top:theme.spacing(2)
+
+    }
 }))
 
 export default function ButtonProfile({onClick:onClick}){
@@ -24,8 +32,17 @@ export default function ButtonProfile({onClick:onClick}){
     }
 
     return(
+        <>
         <Button onClick={(event)=>handleClick(event)} type="button" variant='contained' className={classes.btn} >
-            {user.data.emailVerified?"Perfil":"Verificar Email"}
+            {user.data.emailVerified?"Perfil":"Enviar de nuevo"}
         </Button>
+        <Box className={classes.alert}>
+            {!user.data.emailVerified&&
+                <Alert severity="info">
+                    Mensaje de confirmación de email enviado, por favor checkea tu mail, puede que se encuentre en la casilla de spam, si el correo no fue recibido hay click en “Enviar de nuevo” para enviar el correo de confirmación nuevamente.
+                </Alert>
+            }
+        </Box>
+        </>
     )
 }
