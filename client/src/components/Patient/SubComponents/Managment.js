5394c9e326242851ced8511fb264cf1a411b9ad6
Jorge Gutierrez <jorge.gutierrezl@outlook.com>
2021-08-25 17:32:23 -0500

    mercado-pago

--- client/src/components/Patient/SubComponents/Managment.js
+++ client/src/components/Patient/SubComponents/Managment.js
@@ -1,5 +1,11 @@
-import { Box, Typography, makeStyles } from "@material-ui/core";
+import { Box, Typography, makeStyles, Grid } from "@material-ui/core";
 import devImg from "../../../assets/img/etc/inDev.png"
+//importaciones para mercadopago
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const FORM_ID = 'payment-form';
 
 const useStyle = makeStyles((theme) => ({
     text: {
@@ -21,10 +27,44 @@ const useStyle = makeStyles((theme) => ({
 
 export default function Managment(){
     const classes = useStyle();
+      
+    function handleSummit(){
+        
+    }
     return(
         <Box className={classes.box}>
             <Typography variant='h4' color='blue'>Managment</Typography>
-            <img src={devImg} />
+            
+    <Grid >
+      <Typography variant="h2" component="h2" >Detalle de productos</Typography>
+      <Grid >
+        <Grid >
+          <Grid >
+          <Grid >
+            <Typography variant="h3" component="h3">Operacion</Typography>
+          </Grid>
+            <Grid >
+                <img src="./assets/cardiologia.jfif" alt="foto del producto" ></img>
+                <hr/>
+                <Typography variant="h5" component="h5">Descripcion</Typography> 
+                  <p>Lorem ipsum dolor</p>
+                  <hr/>
+                  <Typography variant="h5" component="h5">Precio $2,500</Typography>
+                  <Grid>
+                                <form action="http://localhost:3001/checkout" method="POST" id={FORM_ID} onSummit={handleSummit}>
+                                    <input type="hidden" name="title" value="Opercacion" />
+                                    <input type="hidden" name="price" value="2500" />
+                                    <input type="submit" value="pagar"  />
+                                    
+                                </form>
+                            </Grid>
+                            <br/><br/><br/>
+              </Grid> 
+          </Grid>
+          </Grid> 
+      </Grid>
+    </Grid>
+  
         </Box>
     )
 }
